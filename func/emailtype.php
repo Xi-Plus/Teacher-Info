1c689d340ea86bfcbf009d77824a44c2ac8a9a18
xiplus <huangxuanyuxiplus@gmail.com>
2017-02-19 22:33:23 +0800

    func alldata

--- func/emailtype.php
+++ func/emailtype.php
@@ -4,8 +4,12 @@ $sth = $G["db"]->prepare('SELECT * FROM `email_type`');
 $sth->execute();
 $row = $sth->fetchAll(PDO::FETCH_ASSOC);
 $D['emailtype'] = array();
+$D['emailtypeall'] = array();
 foreach ($row as $temp) {
 	$D['emailtype'][$temp['id']] = $temp;
+	if ($temp['inuse'] == 1) {
+		$D['emailtypeall'][$temp['id']] = $temp;
+	}
 }
 
 ?>
--- func/teachertype.php
+++ func/teachertype.php
@@ -4,8 +4,12 @@ $sth = $G["db"]->prepare('SELECT * FROM `teacher_type`');
 $sth->execute();
 $row = $sth->fetchAll(PDO::FETCH_ASSOC);
 $D['teachertype'] = array();
+$D['teachertypeall'] = array();
 foreach ($row as $temp) {
 	$D['teachertype'][$temp['id']] = $temp;
+	if ($temp['inuse'] == 1) {
+		$D['teachertypeall'][$temp['id']] = $temp;
+	}
 }
 
 ?>
