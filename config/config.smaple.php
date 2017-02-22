4c31331b1bd6fb35c0da617fb4639c42e251b246
xiplus <huangxuanyuxiplus@gmail.com>
2017-02-22 12:12:27 +0800

    mail config

--- config/config.smaple.php
+++ config/config.smaple.php
@@ -1,5 +1,6 @@
 <?php
 
+$C["domain"] = 'https://sp.tnfsh.tn.edu.tw';
 $C["path"] = '/teacherinfo';
 $C["sitename"] = '公民科教師資料填報';
 $C["titlename"] = '公民教師資料';
@@ -8,6 +9,8 @@ $C["DBhost"] = 'localhost';
 $C["DBuser"] = 'user';
 $C["DBpass"] = 'pass';
 $C["DBname"] = 'dbname';
+
+$C["mail"] = 'no-reply@sp.tnfsh.tn.edu.tw';
 
 $G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8', $C["DBuser"], $C["DBpass"]);
 $G["schoolyear"] = date("Y")-1911-(date("m")<=8);
